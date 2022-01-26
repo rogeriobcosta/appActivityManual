2f5ca574d3f928d906716e6995a06d131011a645
rogeriobcosta <rogeriobc@gmail.com>
2022-01-26 11:40:24 -0300

    primeiro commit, primeira activity manual em java

---
+++ agenda/app/src/main/java/com/rbc/agenda/MainActivity.java
@@ -0,0 +1,22 @@
+package com.rbc.agenda;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView aluno = new TextView(this);
+        aluno.setText("Rogério");
+        setContentView(aluno);
+
+
+    }
+
+
+}
